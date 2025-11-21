7bae25c8f7cac9c5752bb06d18f9a69717e8534f
Daniel Sebastian Spalek <dan.spalek007@gmail.com>
2025-11-21 19:14:00 +0100

    komit test

--- js/script_dodgeIt.js
+++ js/script_dodgeIt.js
@@ -48,7 +48,7 @@
         function handleKeyboardMovement() {
             const LEFT_KEY = 37, RIGHT_KEY = 39, UP_KEY = 38, DOWN_KEY = 40;
             const A_KEY = 65, D_KEY = 68, W_KEY = 87, S_KEY = 83;
-            const moveSpeed = 0.15;
+            const moveSpeed = 0.2;
 
             let vx = 0, vy = 0;
 
